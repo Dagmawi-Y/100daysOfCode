dc3a3a033989a79060679922853b9459b94de629
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-13 09:23:43 +0300

    day 2 challenge 1 done

---
+++ Day-2/challenge-1.py
@@ -0,0 +1,8 @@
+num_str = input("enter a 2 digit number... ")
+
+num1 = int(num_str[0])
+num2 = int(num_str[1])
+
+sum = str(num1 + num2)
+
+print("The sum of the digits is " + sum + ".")
---
+++ Day-2/main.py
@@ -0,0 +1,5 @@
+num_char = len(input("what is your name? "))
+
+converted_num_char = str(num_char)
+
+print("Your name has " + converted_num_char + " characters.")
