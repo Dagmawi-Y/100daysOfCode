410b421d40f4976e6c93c1f9d17a46def1337117
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-13 10:09:26 +0300

    imporoved to show BMI category

--- Day-2/challenge-2.py
+++ Day-2/challenge-2.py
@@ -1,10 +1,22 @@
 weight_str = input('Please enter your weight in kg... ')
-height_str = input('Ok now please enter your height in cm... ')
+height_str = input('Ok now please enter your height in m... ')
 
 weight = float(weight_str)
 height = float(height_str)
 
-BMI = str(round((weight) / (height ** 2), 2))
+BMI = round((weight) / (height ** 2), 2)
+
+if( BMI < 18.5 ):
+    BMI_status = "Under-weight"
+elif BMI > 18.5 and BMI < 25:
+    BMI_status = "Normal-weight"
+elif BMI > 25 and BMI < 30:
+    BMI_status = "Over-weight"
+else:
+    BMI_status = "Obese"
+
+BMI_str = str(round((weight) / (height ** 2), 2))
 
 
-print('your BMI index is: ' + BMI)
+
+print('your BMI index is: ' + BMI_str + ' and your are ' + BMI_status)
