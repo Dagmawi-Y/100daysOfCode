15f4ad5b33f704a03c2f059c51b945444f6375d3
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-14 22:15:48 +0300

    challenge 4 solution optimized

--- Day-3/challenge-4.py
+++ Day-3/challenge-4.py
@@ -12,37 +12,24 @@ size = input("Please enter the pizza size you want. S, M or L?  ")
 peproni = input ("Do you want peproni to be added? type Y or N.  ")
 extra = input ( "Do you want extra cheese to be added? Y or N.  " )
 
-small = 15
-medium = 20
-large = 25
-
-pep_s = 2
-pep_m_l = 3
-
-extra_cheese = 1
-
 total_bill = 0
 
+if size == "S":
+    total_bill += 15
+elif size == "M":
+    total_bill += 20
+elif size == "L":
+    total_bill += 25
+else:
+    print("Please enter a valid size. ")
 
-if (size == "S"):
-    total_bill = small
-if (size == "S" and peproni == "Y"):
-    total_bill = small + pep_s
-if (size == "S" and peproni == "Y" and extra == "Y"):
-    total_bill = small + pep_s + extra_cheese
+if peproni == "Y":
+    if size == "S":
+        total_bill += 2
+    else:
+        total_bill += 3
 
-if (size == "M"):
-    total_bill = medium
-if (size == "M" and peproni == "Y"):
-    total_bill = medium + pep_m_l
-if (size == "S" and peproni == "Y" and extra == "Y"):
-    total_bill = medium + pep_m_l + extra_cheese
-
-if (size == "L"):
-    total_bill = large
-if (size == "L" and peproni == "Y"):
-    total_bill = large + pep_m_l
-if (size == "L" and peproni == "Y" and extra == "Y"):
-    total_bill = large + pep_m_l + extra_cheese
+if extra == "Y":
+    total_bill += 1
 
 print(f"The total bill is: {total_bill}")
