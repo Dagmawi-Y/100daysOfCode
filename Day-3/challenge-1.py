378f4533064a798c14965bb7b67a9cb9129b3254
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-14 19:40:13 +0300

    day-3 challenge 1 done

---
+++ Day-3/challenge-1.py
@@ -0,0 +1,8 @@
+# Odd or even exercies
+
+num = int(input("Please enter a number... "))
+
+if num % 2 == 0:
+    print(f"{num} is an even number. ")
+else:
+    print(f"{num} is an odd number.")
---
+++ Day-3/main.py
@@ -0,0 +1,7 @@
+print ("Welcome to the roller coaster!")
+height = int(input("What is your height in cm?"))
+
+if height < 120:
+    print("You are not allowed to use the rollercosater.")
+else:
+    print("Welcome to the roller coaster")
