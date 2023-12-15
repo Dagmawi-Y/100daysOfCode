a9f8ea76c3cf3d6a6c894646d05a89c4366f38a1
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-15 14:14:31 +0300

    challenge 1 init

---
+++ Day-4/main.py
@@ -0,0 +1,13 @@
+import random
+import my_module
+
+random_integer = random.randint(1, 10)
+
+print(random_integer)
+print(my_module.pi)
+
+random_float = random.random()
+print(random_float)
+
+random_float_upto_5 = random.random() * 5
+print(random_float_upto_5)
