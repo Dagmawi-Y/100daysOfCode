6dd0a8edf7701f9e29ac84a21af4f514a61af3f4
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-16 00:36:09 +0300

    challenge 3 init

--- Day-4/challenge-2.py
+++ Day-4/challenge-2.py
@@ -1,11 +1,19 @@
-import random
+# import random
 
-names = input ("Please enter the names separted by comma... ")
+# names = input ("Please enter the names separted by comma... ")
 
-names_split = names.split(",")
+# names_split = names.split(",")
 
-length_of_names = len(names_split)
+# length_of_names = len(names_split)
 
-random_payer = random.randint(0, length_of_names - 1)
+# random_payer = random.randint(0, length_of_names - 1)
 
-print (f"{names_split[random_payer]} is going to pay the bill today. ")
+# print (f"{names_split[random_payer]} is going to pay the bill today. ")
+
+
+list_1 = ["1", "2", "3"]
+list_2 = ["4", "5", "6"]
+
+nested_list = [list_1, list_2]
+
+print(nested_list)
