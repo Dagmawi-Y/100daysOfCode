f03d8124fa094911035d0bc7165ebadb11767521
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-15 23:47:58 +0300

    challenge 2 init

---
+++ Day-4/challenge-1.py
@@ -0,0 +1,11 @@
+import random
+
+heads = "Heads"
+tails = "Tails"
+
+randomNumber = random.randint(0, 1)
+
+if randomNumber == 0:
+    print ("Heads")
+else:
+    print ("Tails")
--- Day-4/main.py
+++ Day-4/main.py
@@ -1,13 +1,19 @@
 import random
 import my_module
 
-random_integer = random.randint(1, 10)
+# random_integer = random.randint(1, 10)
 
-print(random_integer)
-print(my_module.pi)
+# print(random_integer)
+# print(my_module.pi)
 
-random_float = random.random()
-print(random_float)
+# random_float = random.random()
+# print(random_float)
 
-random_float_upto_5 = random.random() * 5
-print(random_float_upto_5)
+# random_float_upto_5 = random.random() * 5
+# print(random_float_upto_5)
+
+states_of_america = []
+
+states_of_america.append("DC")
+
+print(states_of_america[0])
