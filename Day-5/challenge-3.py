d5d597296823b1cf5a1af47958a97d4dae114930
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-17 00:39:15 +0300

    challenge 4 done

---
+++ Day-5/challenge-3.py
@@ -0,0 +1,6 @@
+sum = 0
+
+for n in range(2, 101, 2):
+    sum += n
+
+print(f"The sum of all even numbers between 1 and 100 is: {sum}")
--- Day-5/main.py
+++ Day-5/main.py
@@ -1,4 +1,11 @@
-fruits = ["apple", "banana", "orange"]
+# fruits = ["apple", "banana", "orange"]
 
-for fruit in fruits:
-    print(fruit)
+# for fruit in fruits:
+#     print(fruit)
+
+sum = 0
+
+for n in range(1, 101):
+    sum += n
+    
+print(f"The sum of numbers between 1 and 100 is: {sum}")
