8c170803f93d057c510e2b20b9c6b2a2197f4878
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-17 11:57:26 +0300

    challenge 1 done

---
+++ Day-6/challenge-1.py
@@ -0,0 +1,6 @@
+'''
+Reeborg World's Hurdle Challenge done.
+
+"Hurdle Challengehttps://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json"
+
+'''
