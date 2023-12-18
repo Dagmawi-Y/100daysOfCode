e4a06cad4bc5e201b7525d0b6781ea617fb4aa88
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-18 13:34:31 +0300

    project done

---
+++ Day-7/hangman_final.py
@@ -0,0 +1,59 @@
+#Step 5
+import random
+import hangman_words
+import hangman_art
+
+#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+logo = hangman_art.logo
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    previous_guess = guess
+
+    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess == previous_guess:
+        print("You have already guessed this letter.")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print("The letter you guessed is not found in the word. you lost a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    print(hangman_art.stages[lives])
+    if (lives == 0):
+        print("You lose.")
