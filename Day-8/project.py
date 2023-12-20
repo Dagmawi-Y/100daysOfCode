aa65766f8933aa68d14851098642c77a20730b89
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-20 23:53:54 +0300

    project part 2 done

--- Day-8/project.py
+++ Day-8/project.py
@@ -23,4 +23,22 @@ def encrypt(text, shift):
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(text, shift)
+def decrypt(text, shift):
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+    shifted_text = ""
+    for letter in text:
+        index = alphabet.index(letter)
+        shifted_text += alphabet[index - shift]
+    print(f"The encoded text is: {shifted_text}")
+
+if direction == "encode":
+    encrypt(text, shift)
+else:
+    decrypt(text, shift)
