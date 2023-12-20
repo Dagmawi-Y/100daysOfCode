f6238128c7099d07632617082f1087483bad7867
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-20 22:19:59 +0300

    challenge 1 init

---
+++ Day-8/main.py
@@ -0,0 +1,15 @@
+# def greet():
+#     print("Hello without name.")
+
+# greet()
+
+# def greet_with_name(name):
+#     print (f"Hello {name}")
+
+# greet_with_name("dada")
+
+def greet_with(fname, lname):
+    print(f"Hello {fname}{lname}")
+
+
+greet_with(fname = "dada", lname = "john")
