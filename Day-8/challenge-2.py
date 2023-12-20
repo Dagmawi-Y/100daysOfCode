3fb68df956eb8fc90dc42cb6660a47cf78b8fa45
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-20 22:57:13 +0300

    challenge 2 done

---
+++ Day-8/challenge-2.py
@@ -0,0 +1,13 @@
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number - 1):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print(f"{number} is a prime number.")
+    else:
+        print(f"{number} is not a prime number.")
+
+check_num = int(input("Enter the number:  "))
+
+prime_checker(check_num)
