52ba5620051a001678dc2b363f8aba3f92588343
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-24 00:16:22 +0300

    project done

--- Day-10/project.py
+++ Day-10/project.py
@@ -1,3 +1,6 @@
+import os
+import art
+
 def add(x, y):
     return x + y
 
@@ -17,17 +20,25 @@ operations = {
     "/": divide
 }
 
-x = int(input("What is the first number? "))
+def calculate():
+    print(art.logo)
+    x = float(input("What is the first number? "))
+    for operation in operations:
+        print(operation)
+        should_continue = True
 
-for operation in operations:
-    print(operation)
+    while should_continue:
+        chosen_op = input("Pick an operation: ")
+        y = float(input("What is the next number? "))
+        calc_function = operations[chosen_op]
+        answer = calc_function(x, y)
+        print (f"{x} {chosen_op} {y} = {answer}")
 
-chosen_op = input("Choose an operation from the list above: ")
+        if (input(f"Type 'y' to keep calculating with {answer}, or type n to start a new calculation.")) == "y":
+            x = answer
+        else:
+            should_continue = False
+            os.system("cls")
+            calculate()
 
-y = int(input("What is the second number? "))
-
-calc_function = operations[chosen_op]
-
-answer = calc_function(x, y)
-
-print (f"{x} {chosen_op} {y} = {answer}")
+calculate()
