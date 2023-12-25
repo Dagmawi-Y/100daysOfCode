95148ad0561ebe6698367a703c51ce0831373417
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-25 22:34:01 +0300

    blackjack fix

--- Day-14/main.py
+++ Day-14/main.py
@@ -16,9 +16,7 @@ def format_data(account):
   return f"{name}, a {description}, from {country}"
 
 def check_answer(guess, a_followers, b_followers):
-  """Checks followers against user's guess 
-  and returns True if they got it right.
-  Or False if they got it wrong.""" 
+  """Checks followers against user's guess and returns True if they got it right. Or False if they got it wrong.""" 
   if a_followers > b_followers:
     return guess == "a"
   else:
