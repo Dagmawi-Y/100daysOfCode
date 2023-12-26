a4404d4ea486795bac23309bde60e099fcee93ef
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-26 23:57:53 +0300

    package installation done and gui worked

---
+++ Day-16-OOP/main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+from prettytable import PrettyTable
+
+# daggy = Turtle()
+# my_screen = Screen()
+#
+# print(daggy)
+# daggy.shape('turtle')
+# daggy.color('aquamarine')
+# daggy.forward(100)
+#
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+table = PrettyTable()
+
+table.add_column("City name",
+["Adelaide","Brisbane","Darwin","Hobart","Sydney","Melbourne","Perth"])
+table.add_column("Area", [1295, 5905, 112, 1357, 2058, 1566, 5386])
+table.add_column("Population", [1158259, 1857594, 120900, 205556, 4336374, 3806092,
+1554769])
+table.add_column("Annual Rainfall",[600.5, 1146.4, 1714.7, 619.5, 1214.8, 646.9,
+869.4])
+table.align = "l"
+print(table)
