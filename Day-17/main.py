d42fe9c0984706c579ee335a6642d7c3348f2562
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-12-28 01:18:32 +0300

    day 17 project done

--- Day-17/main.py
+++ Day-17/main.py
@@ -1,21 +1,40 @@
-class User:
-    def __init__(self, username):
-        self.username = username
-        self.followers = 0
-        self.following = 0
+# class User:
+#     def __init__(self, username):
+#         self.username = username
+#         self.followers = 0
+#         self.following = 0
+#
+#     def print_user_name(self):
+#         print(self.username)
+#
+#     def follow_user(self, user):
+#         self.following += 1
+#         user.followers += 1
+#
+#
+# user_1 = User(username="dag")
+#
+# user_2 = User(username="yoh")
+#
+# user_1.follow_user(user_2)
+#
+#
 
-    def print_user_name(self):
-        print(self.username)
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
 
-    def follow_user(self, user):
-        self.following += 1
-        user.followers += 1
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
 
-user_1 = User(username="dag")
+while quiz.still_has_questions():
+    quiz.next_question()
 
-user_2 = User(username="yoh")
-
-user_1.follow_user(user_2)
-
-
+print("You've completed the quiz")
+print(f"You're final score is {quiz.score}/{quiz.question_number}")
