f632196e69bd736a30d551a3640bb6b91ec891be
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-01-01 23:38:35 +0300

    Snake wall collision detection feature added.

---
+++ Day-21-snake_game_2/scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 250)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increment_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over", align=ALIGNMENT, font=FONT)
