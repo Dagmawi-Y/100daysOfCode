3439c8eb83e905268b1afc50a3d44ee660319c4c
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-01-02 22:27:39 +0300

    day 22 polished & day 23 init

--- Day-22-pong/ball.py
+++ Day-22-pong/ball.py
@@ -10,7 +10,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 3
         self.y_move = 3
-        self.move_speed = 0.1
+        self.move_speed = 0.03
 
     def move(self):
         new_x = self.xcor() + self.x_move
--- Day-22-pong/main.py
+++ Day-22-pong/main.py
@@ -1,3 +1,4 @@
+import turtle
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
@@ -22,24 +23,26 @@ screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
+
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
-    #Detect collision with wall
+    # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    #Detect collision with paddle
+    # Detect collision with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    #Detect R paddle misses
+    # Detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
         scoreboard.l_point()
 
-    #Detect L paddle misses:
+    # Detect L paddle misses:
     if ball.xcor() < -380:
         ball.reset_position()
         scoreboard.r_point()
