d81b2e9a8b468e6a158dc134ce87ae73c5b2ddab
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-01-19 03:39:22 +0300

    day 33 done

--- Day-33/main.py
+++ Day-33/main.py
@@ -1,8 +1,30 @@
 import requests
+import datetime as dt
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 48.937470
+MY_LON = 2.461450
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# lat = data["iss_position"]["latitude"]
+# lon = data["iss_position"]["longitude"]
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LON,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-lat = data["iss_position"]["latitude"]
-lon = data["iss_position"]["longitude"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = dt.datetime.now()
+# print(time_now)
